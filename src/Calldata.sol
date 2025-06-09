3be4e48d34e7917e898ba8d56fe8666784e26b90
Raoul <1412344+RaoulSchaffranek@users.noreply.github.com>
2025-06-09 10:08:54 +0000

    Add remaining examples

--- src/Calldata.sol
+++ src/Calldata.sol
@@ -8,10 +8,24 @@ contract Calldata {
         this.encodeFirst(1,2,3);
     }
 
-    function encodeFirst(uint256 a,uint256 b, uint256 c) pure external returns (uint256) {
-        uint256 result = a + b + c;
-        return result;
+    function encodeFirst(uint256 a,uint256 b, uint256 c) pure external returns (bytes memory) {
+        return msg.data;
     }
 
+    function call_encodeSecond() external {
+        this.encodeSecond("DecipherClub");
+    }
+
+    function encodeSecond(string memory s) external pure returns(bytes memory) {
+        return msg.data;
+    }
+
+    function call_encodeThird() external {
+        this.encodeThird(786, 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, "DecipherClub");
+    }
+
+    function encodeThird(uint256 a, address b, string memory c) external pure returns (bytes memory) {
+        return msg.data;
+    }
 
 }
